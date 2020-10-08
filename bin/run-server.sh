3f27279b6b4baa3689cc2f79b186cc391a60e983
Tadej Gregorcic <tadej.gregorcic@gmail.com>
2020-10-08 23:08:49 +0200

    chore: changed server frequency to 60Hz

--- bin/run-server.sh
+++ bin/run-server.sh
@@ -1,1 +1,1 @@
-../coherence-bin/coherence-replication-server serve --port 12345 --frequency 10 --schema ../Assets/coherence/coherence.schema --entities ../Assets/coherence/world.yaml --log-level DEBUG
+../coherence-bin/coherence-replication-server serve --port 12345 --frequency 60 --schema ../Assets/coherence/coherence.schema --entities ../Assets/coherence/world.yaml --log-level DEBUG
