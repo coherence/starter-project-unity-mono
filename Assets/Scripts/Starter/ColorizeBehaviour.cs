e76a8712f2f62ea9dbb45e4c6f3a3924060ed825
Francisco Requena <frarees@gmail.com>
2020-10-28 16:37:51 +0100

    feat: simplify and clean starter kit

---
+++ Assets/Scripts/Starter/ColorizeBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ColorizeBehaviour : MonoBehaviour
+{
+    public Material material;
+    public new Renderer renderer;
+
+    private void Start()
+    {
+        renderer.material = material;
+    }
+}
---
+++ Assets/Scripts/Starter/MovementBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed;
+
+    private void Reset()
+    {
+        characterController = GetComponent<CharacterController>();
+        speed = 2f;
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0f, v);
+        move = Camera.main.transform.TransformDirection(move);
+        move.y = 0f;
+
+        _ = characterController.Move(move * speed * Time.smoothDeltaTime);
+    }
+}
