e07e3619b008f8bcf1c9c06854d79a502d064918
Cary Brisebois <68713879+cbrisebois@users.noreply.github.com>
2020-10-30 05:03:12 -0700

    fix: log blowing up Text because the string is too long (#16)

--- Assets/Scripts/DebugPanel.cs
+++ Assets/Scripts/DebugPanel.cs
@@ -7,4 +7,14 @@ public class DebugPanel : MonoBehaviour
 {
     public Text log;
     public InputField stringToSend;
+
+    private readonly int MAX_CHARS = 1280;
+    private readonly string NEW_LINE = "\r\n";
+
+    public void AddLog(string logString)
+    {
+        var newText = logString + NEW_LINE + log.text;
+        newText = newText.Substring(0, Mathf.Min(newText.Length, MAX_CHARS));
+        log.text = newText;
+    }
 }
