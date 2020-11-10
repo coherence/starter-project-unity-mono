a1ed0cdcb3a6695671b68a09f59b345bdc4f5a99
Francisco Requena <frarees@gmail.com>
2020-11-10 11:10:53 +0100

    feat: updated to the new sdk (#23)

--- Assets/Coherence Samples/Interface/Scripts/Server.cs
+++
@@ -1,9 +0,0 @@
-namespace Coherence.Samples
-{
-    public static class Server
-    {
-        // this will be patched by the build workflow
-        // in case of changes please fix the workflow accordingly
-        public static string IP = "127.0.0.1:12345";
-    }
-}
