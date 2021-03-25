77e7ccfca29fca355a893f6c95e33dfdd519355d
Erik Svedäng <erik@coherence.io>
2021-03-25 14:02:51 +0100

    Some preparation.

---
+++ Assets/Coherence/Baked/CoherenceSyncCube.cs
@@ -0,0 +1,138 @@
+
+
+
+#region CoherenceSyncCube
+// -----------------------------------
+//  CoherenceSyncCube.cs
+// -----------------------------------
+			
+namespace Coherence.Generated
+{
+	using UnityEngine;
+	using Unity.Collections;
+	using Unity.Entities;
+	using Unity.Mathematics;
+	using Unity.Transforms;
+	using Coherence.Toolkit;
+	using Coherence.Replication.Client.Unity.Ecs;
+	using static Coherence.Toolkit.CoherenceSync;
+	using global::Coherence.Generated.Internal;
+
+	public class CoherenceSyncCube : CoherenceSyncBaked
+	{
+		private CoherenceSync coherenceSync;
+		private EntityManager entityManager;
+		private bool componentsInitialized = false;
+
+		// Cached references to MonoBehaviours on this GameObject
+		
+
+		protected void Awake()
+		{
+			entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+			coherenceSync = GetComponent<CoherenceSync>();
+			coherenceSync.usingReflection = false;
+
+			
+
+			
+
+			coherenceSync.OnSpawnFromNetwork += OnSpawnFromNetwork;
+		}
+
+		private void OnSpawnFromNetwork()
+		{
+			InitializeComponents();
+			SyncEcsBaked();
+		}
+
+		public override void InitializeComponents()
+		{
+			var entity = coherenceSync.LinkedEntity;
+
+			if(coherenceSync.HasArchetype) {
+				entityManager.AddComponent<LastObservedLod>(entity);
+			}
+
+			if (!coherenceSync.isSimulated) return;
+
+			
+
+			if (coherenceSync.HasArchetype)
+			{
+				int archetypeIndex = Archetype.IndexForName[coherenceSync.Archetype.ArchetypeName];
+				entityManager.AddComponentData(entity, new ArchetypeComponent { index = archetypeIndex });
+			}
+
+			if (coherenceSync.lifetimeType == CoherenceSync.LifetimeType.Persistent)
+			{
+				entityManager.AddComponentData(entity, new Persistence()
+				{
+					uuid = coherenceSync.persistenceUUID,
+					expiry = coherenceSync.GetPersistenceExpiryString()
+				});
+			}
+
+			if (coherenceSync.authorityTransferType != CoherenceSync.AuthorityTransferType.NotTransferable)
+			{
+				entityManager.AddComponent<AuthorityTransfer>(entity);
+			}
+
+			entityManager.AddComponent<Simulated>(entity);
+
+			componentsInitialized = true;
+		}
+
+		void Update()
+		{
+			if (!coherenceSync.EcsEntityExists())
+			{
+				return;
+			}
+
+			if (!componentsInitialized)
+			{
+				InitializeComponents();
+			}
+
+			
+
+			SyncEcsBaked();
+		}
+
+		
+
+		static FixedString64 ObjectToFixedString64(object o)
+		{
+			return new FixedString64((string)o);
+		}
+
+		static float3 Vector3ToFloat(object o)
+		{
+			Vector3 v = (Vector3)o;
+			return new float3(v.x, v.y, v.z);
+		}
+
+		private void SyncEcsBaked()
+		{
+			var entity = coherenceSync.LinkedEntity;
+
+			if (coherenceSync.isSimulated)
+			{
+				
+			}
+			else
+			{
+				
+
+				if(coherenceSync.HasArchetype) {
+					int level = entityManager.GetComponentData<LastObservedLod>(entity).Level;
+					coherenceSync.Archetype.SetObservedLodLevel(level);
+				}
+			}
+		}
+	}
+}
+
+// ------------------ end of CoherenceSyncCube.cs -----------------
+#endregion
---
+++ Assets/Starter/Scripts/Cube.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Coherence.Toolkit;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    static Cube previousCube;
+
+    CoherenceSync sync;
+    public CoherenceSync friend;
+
+    void Start()
+    {
+        sync = GetComponent<CoherenceSync>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        //GUI.Label()
+    }
+
+    //private void OnMouseDown()
+    //{
+    //    Debug.Log($"User clicked {name} (simulated = {sync.isSimulated})");
+
+    //    if(sync.isSimulated)
+    //    {
+    //        if(previousCube == null)
+    //        {
+    //            previousCube = this;
+    //        }
+    //        else
+    //        {
+    //            friend = previousCube.sync;
+    //            previousCube = null;
+    //        }
+    //    }        
+    //}
+}
